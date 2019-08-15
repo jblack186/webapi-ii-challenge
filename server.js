ee9c7618fbf8ebf4d1c5ccb8a2c4e655fbb83424
Jamison Blackwell <blackwellj1040@gmail.com>
2019-08-15 19:33:49 -0400

    test

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ server.use(express.json());
 server.use('/api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
-    db.get()
-    .then(posts => {
-        res.status(200).json(posts);
-    })
+   res.send(console.log('yo'))
     .catch (error => {
         console.log(error);
         res.status(500).json({
