e64ebc08ac302b780c519d9b95cc2b94e00401af
Jamison Blackwell <blackwellj1040@gmail.com>
2019-08-15 19:36:08 -0400

    another test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const PostsRouter = require('./hubs/posts-router.js');
 
-const db = require('./data/db.js');
+const db = require('../data/db.js');
 
 const server = express();
 
@@ -11,7 +11,10 @@ server.use(express.json());
 server.use('/api/posts', PostsRouter);
 
 server.get('/', (req, res) => {
-   res.send(console.log('yo'))
+    db.find()
+    .then(posts => {
+        res.status(200).json(posts);
+    })
     .catch (error => {
         console.log(error);
         res.status(500).json({
